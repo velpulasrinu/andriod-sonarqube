453f62cd119c8f53fa6f4effeb6a534ed7439a82
Velpula Srinivasa Rao <SrinivasaRao.Velpula@ivycomptech.com>
2022-12-13 10:55:39 +0530

    andriod files

---
+++ README.md
@@ -0,0 +1,31 @@
+# Sample application
+
+This application demonstrate how to configure Sonarqube with gradle for an Android project.
+Sources are based on a Google sample app.
+
+# How to analyze this project
+- Install [Docker](https://www.docker.com)
+
+- Install and run [Sonqube Docker image](https://store.docker.com/images/3f8fc4ce-eb8e-40ad-88ba-69e97299c64f?tab=description) `sonarqube:alpine`:
+  ```
+  docker run -d -p 9000:9000 -p 9092:9092 sonarqube:alpine
+  ```
+
+`sonarqube:alpine` is a lightweight version (5M) or sonarqube:latest. See [docker store page](https://store.docker.com/images/3f8fc4ce-eb8e-40ad-88ba-69e97299c64f?tab=description) for details.
+
+- Run analysis
+```
+./gradlew clean test jacocoTestReport sonarqube \
+          -Dsonar.host.url=http://[Sonarqube container IP]:9000 \
+          --info --stacktrace
+```
+Replace `[Sonarqube container IP]` with corresponding IP address.
+
+- Watch result here: http://[Sonarqube container IP]:9000
+  * Log in with default user: `admin/admin`
+  * **Warning**: analyses is done by a background task and can take a view seconds. You can follow background task here: `Administration > Projects > Background Tasks`
+
+- See Sonarqube logs:
+```
+docker logs sonarqube:alpine
+```
---
+++ android-app/src/androidTest/java/com/sogilis/example/android/app/MainActivityTest.java
@@ -0,0 +1,46 @@
+package com.sogilis.example.android.app;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.WindowManager;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void unlockScreen() {
+        final MainActivity activity = mActivityTestRule.getActivity();
+        Runnable wakeUpDevice = new Runnable() {
+            public void run() {
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        };
+        activity.runOnUiThread(wakeUpDevice);
+    }
+
+    @Test
+    public void mainActivityTest() {
+        onView(withId(R.id.result_text)).check(matches(withText(equalTo("..."))));
+        onView(withId(R.id.compute_button)).perform(click());
+        onView(withId(R.id.result_text)).check(matches(withText(not(equalTo("...")))));
+    }
+}
